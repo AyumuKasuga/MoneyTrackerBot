cbe6d0ff327b0747edab6bfbdf9fce7d6543a5ed
Andrey Kostakov <bps@dzen.ws>
2016-08-07 03:35:18 +0300

    put categories to config

--- bot.py
+++ bot.py
@@ -11,6 +11,15 @@ from telepot.namedtuple import ReplyKeyboardMarkup, KeyboardButton, ReplyKeyboar
 from storage import MoneyTrackerStorage
 
 
+def make_keyboard(lists):
+    keyboard = [[] for _ in range(len(lists))]
+    for idx, row in enumerate(lists):
+        for label in row:
+            keyboard[idx].append(KeyboardButton(text=label))
+    keyboard.append([KeyboardButton(text='/cancel')])
+    return keyboard
+
+
 class MoneyTrackerBot(telepot.aio.Bot):
 
     def __init__(self, *args, config=None, st=None, **kwargs):
@@ -64,40 +73,64 @@ class MoneyTrackerBot(telepot.aio.Bot):
             self.loop.create_task(self.send_total(chat_id))
         elif msg['text'].startswith('/add'):
             self.sessions[chat_id] = {}
-            self.loop.create_task(self.sendMessage(chat_id, '\U0001f4b8 Please enter sum that you just spent', reply_markup=ReplyKeyboardHide()))
+            self.loop.create_task(
+                self.sendMessage(
+                    chat_id,
+                    '\U0001f4b8 Please enter sum that you just spent',
+                    reply_markup=ReplyKeyboardHide()
+                )
+            )
         elif msg['text'].startswith('/cancel'):
             if chat_id in self.sessions:
                 self.sessions.pop(chat_id)
-            self.loop.create_task(self.sendMessage(chat_id, 'Okay, forgot everything \U0001f642', reply_markup=ReplyKeyboardHide()))
+            self.loop.create_task(
+                self.sendMessage(
+                    chat_id,
+                    'Okay, forgot everything \U0001f642',
+                    reply_markup=ReplyKeyboardHide()
+                )
+            )
         else:
             if chat_id not in self.sessions:
                 self.loop.create_task(self.sendMessage(chat_id, '\U0001f914'))
             elif len(self.sessions[chat_id]) == 0:
                 if msg['text'].isdigit():
-                    markup = ReplyKeyboardMarkup(keyboard=[
-                        [KeyboardButton(text='\U0001f354 \U0001f35f'), KeyboardButton(text='\U0001f373'), KeyboardButton(text='\U0001f4b0')],
-                        [KeyboardButton(text='\U0001f485'), KeyboardButton(text='\U0001f455'), KeyboardButton(text='\U0001f48a')],
-                        [KeyboardButton(text='\U0001f3ac \U0001f46f'), KeyboardButton(text='\U0001f6bd\U0001f6c1'), KeyboardButton(text='\U0001f3e0')],
-                        [KeyboardButton(text='\U0001f47e')],
-                        [KeyboardButton(text='/cancel')]
-                    ], resize_keyboard=True, one_time_keyboard=True)
-                    self.loop.create_task(self.sendMessage(chat_id, '\U0001f4dd Okay! Now select a category', reply_markup=markup))
+                    markup = ReplyKeyboardMarkup(
+                        keyboard=make_keyboard(self.config.get("categories")),
+                        resize_keyboard=True,
+                        one_time_keyboard=True
+                    )
+                    self.loop.create_task(self.sendMessage(
+                        chat_id,
+                        '\U0001f4dd Okay! Now select a category',
+                        reply_markup=markup)
+                    )
                     self.sessions[chat_id].update({'sum': msg['text']})
                 else:
-                    self.loop.create_task(self.sendMessage(chat_id, '\u274c Please enter correct sum (only digest without dots)'))
+                    self.loop.create_task(self.sendMessage(
+                        chat_id,
+                        '\u274c Please enter correct sum (only digest without dots)'
+                    ))
             elif not self.sessions[chat_id].get('category'):
                 self.sessions[chat_id].update({'category': msg['text']})
-                markup = ReplyKeyboardMarkup(keyboard=[
-                    [KeyboardButton(text='\U0001f4b8')],
-                    [KeyboardButton(text='Lunch'), KeyboardButton(text='Vegetables'), KeyboardButton(text='Supermarket')],
-                    [KeyboardButton(text='/cancel')]
-                ], resize_keyboard=True, one_time_keyboard=True)
-                self.loop.create_task(self.sendMessage(chat_id, 'Okay! Now write or select description', reply_markup=markup))
+                markup = ReplyKeyboardMarkup(
+                    keyboard=make_keyboard(self.config.get("descriptions")),
+                    resize_keyboard=True,
+                    one_time_keyboard=True
+                )
+                self.loop.create_task(self.sendMessage(
+                    chat_id,
+                    'Okay! Now write or select description',
+                    reply_markup=markup
+                ))
             elif not self.sessions[chat_id].get('description'):
                 self.sessions[chat_id].update({'description': msg['text']})
                 data = self.sessions.pop(chat_id)
                 with (await self.lock):
-                    self.loop.run_in_executor(None, functools.partial(self.save_entry, chat_id, data))
+                    self.loop.run_in_executor(
+                        None,
+                        functools.partial(self.save_entry, chat_id, data)
+                    )
 
 
 def bye(signame):
@@ -112,7 +145,10 @@ loop = asyncio.get_event_loop()
 for signame in ('SIGTERM', 'SIGINT'):
     loop.add_signal_handler(getattr(signal, signame), functools.partial(bye, signame))
 token = config.pop("telegram_token")
-st = MoneyTrackerStorage(keyfile='conf/MoneyTracker.json', spreadsheet_name=config["spreadsheet_name"])
+st = MoneyTrackerStorage(
+    keyfile='conf/MoneyTracker.json',
+    spreadsheet_name=config["spreadsheet_name"]
+)
 bot = MoneyTrackerBot(token=token, config=config, st=st, loop=loop)
 loop.create_task(bot.message_loop())
 print("pid %s listening..." % os.getpid())
